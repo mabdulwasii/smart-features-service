c9e696809650439872d1f835334788568b3b0d2b
Princely <mabdulwasii@gmail.com>
2023-02-02 20:33:11 +0100

    Add Enum constraint

---
+++ src/main/java/ai/smart/ix/utils/EnumValueValidatorConstraint.java
@@ -0,0 +1,26 @@
+package ai.smart.ix.utils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EnumValueValidatorConstraint implements ConstraintValidator<ValidEnumValue, CharSequence> {
+    private List<String> values;
+
+    @Override
+    public void initialize(ValidEnumValue validEnumValue) {
+        values = Stream.of(validEnumValue.enumClass().getEnumConstants())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        return values.contains(value.toString().toUpperCase());
+    }
+}
